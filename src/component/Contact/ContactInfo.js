3d2de3738c84e3b26beb298bbf8e27cfb4082ecd
Vlad Maniac <maniacvld@gmail.com>
2023-01-30 18:40:03 +0200

    Init

---
+++ src/component/Contact/ContactInfo.js
@@ -0,0 +1,26 @@
+import React from 'react'
+//ContactInfo Area
+const ContactInfo = () => {
+    return (
+        <>
+            <div className="contact_info_wrapper">
+                <div className="contact_item">
+                    <h5>Our Address</h5>
+                    <p>Via Pave 4, Milano, 20056</p>
+                </div>
+                <div className="contact_item">
+                    <h5>Call us on</h5>
+                    <p>Office: +39 388 854 5019 & +39 388 391 3246</p>
+                    <p>Customer Care: +39 388 391 3246</p>
+                </div>
+                <div className="contact_item">
+                    <h5>Mail Us at</h5>
+                    <p>office@imperialtraslochi.it</p>
+                    <p>deposito@imperialtraslochi.it</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ContactInfo
